a608ef60833607c1b07b00cb2aff716c67c85cd4
kboul <kos.voulgaridis@gmail.com>
2019-04-03 15:29:15 +0300

    Create a btn & toggle ModalComp for proof of concept

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
+import ModalComp from '../common/ModalComp'
 
 class App extends Component {
+    state = {
+        isVisible: false
+    }
+
+    toggleModal = () => {
+        this.setState({ isVisible: !this.state.isVisible })
+    }
+
     render() {
         return (
-            <div>Hello World</div>
+            <div>
+                <button className="btn btn-primary" onClick={this.toggleModal}>
+                    Register Tank
+            </button>
+                <ModalComp
+                    modal={this.state.isVisible}
+                    toggle={this.toggleModal}>
+                    <div>modal content</div>
+                </ModalComp>
+            </div>
+
         )
     }
 }
