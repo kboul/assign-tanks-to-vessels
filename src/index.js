8cab873edf751a0dd2865517f90817fcc5772756
kboul <kos.voulgaridis@gmail.com>
2019-04-03 16:45:17 +0300

    Create & configure redux boilerplate & redux-form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { Provider } from 'react-redux'
+import { createStore, compose } from 'redux'
+import reducer from './redux-local/reducers'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    || compose
+
+const store = createStore(reducer, composeEnhancers())
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'))
 
 serviceWorker.unregister()
