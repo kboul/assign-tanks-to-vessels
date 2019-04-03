ab393c9b71f20470d19d5233014d0d43350a47cb
kboul <kos.voulgaridis@gmail.com>
2019-04-03 15:28:48 +0300

    Create a ModalComp

---
+++ src/common/ModalComp.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Modal, ModalHeader, ModalBody } from 'reactstrap'
+import PropTypes from 'prop-types'
+
+const ModalComp = ({ modal, toggle, children }) => {
+    return (
+        <Modal
+            isOpen={modal}
+            toggle={toggle}
+            size="lg">
+            <ModalHeader toggle={toggle}></ModalHeader>
+            <ModalBody>{children}</ModalBody>
+        </Modal>
+    )
+}
+
+ModalComp.propTypes = {
+    modal: PropTypes.bool.isRequired,
+    toggle: PropTypes.func.isRequired,
+    children: PropTypes.element.isRequired
+}
+
+export default ModalComp
