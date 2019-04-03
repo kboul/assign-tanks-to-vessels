d6ddf055756872a90a2eb3b7e05af4ba0717ae05
kboul <kos.voulgaridis@gmail.com>
2019-04-03 16:53:48 +0300

    Create constant tankLabels

---
+++ src/constants/tankLabels.js
@@ -0,0 +1,34 @@
+export const inputLabels = [
+    { name: 'unimedIn', label: 'Unimed Id' },
+    { name: 'cyclinderSerialNumber', label: 'Cylinder Serial Number' }
+]
+
+export const selectLabels = [
+    {
+        name: 'cylinderSize',
+        label: 'Cylinder Size',
+        options: [
+            'MOX-40',
+            'MOX-10',
+            'MOX-5',
+            'MOX-2'
+        ]
+    },
+    {
+        name: 'origin',
+        label: 'Origin',
+        options: [
+            'US',
+            'EU',
+            'OTHER'
+        ]
+    },
+    {
+        name: 'owner',
+        label: 'Owner',
+        options: [
+            'UNIMED',
+            'OTHER'
+        ]
+    }
+]
