b9149e812498e1420249e069a3ee6797a545bc85
kboul <kos.voulgaridis@gmail.com>
2019-04-03 16:56:22 +0300

    Create reusable Input comp

---
+++ src/common/Input.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Field } from "redux-form"
+import PropTypes from 'prop-types'
+
+const renderField = ({ input }) => {
+    return (
+        <input
+            className='form-control'
+            {...input}
+            type='text' />
+    )
+}
+
+const Input = ({ name, label }) => {
+    return (
+        <div>
+            <label>{label}</label>
+            <div className="form-group">
+                <Field
+                    name={name}
+                    component={renderField}
+                    type="text"
+                />
+            </div>
+        </div>
+    )
+}
+
+Input.propTypes = {
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired
+}
+
+export default Input
