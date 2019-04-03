6c8a8e176f93330de0f81885630ff23f3c968c62
kboul <kos.voulgaridis@gmail.com>
2019-04-03 19:13:10 +0300

    Create a registerTank action to push new registered Tanks

--- src/redux-local/actions/index.js
+++ src/redux-local/actions/index.js
@@ -3,3 +3,8 @@ import { TYPES } from './types'
 export const toggleModal = () => ({
     type: TYPES.TOGGLE_MODAL
 })
+
+export const registerTank = tank => ({
+    type: TYPES.REGISTER_TANK,
+    tank
+})
