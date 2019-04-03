f9579a47f06039a6f87526c60d3a84bd4e9072c3
kboul <kos.voulgaridis@gmail.com>
2019-04-03 23:13:24 +0300

    Create TankList comp & visualize list of tanks with registered info

--- src/components/TankList.js
+++ src/components/TankList.js
@@ -1,10 +1,42 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { allLabels } from '../constants/tankLabels'
+import '../styles/TankList.css'
 
 const TankList = ({ registeredTanks }) => {
     console.log(registeredTanks)
     return (
-        <div>Tank List</div>
+        <table className="table col-6 mt-4">
+            <thead>
+                <tr>
+                    {allLabels.map((label, key) => {
+                        return (
+                            <th
+                                key={key}
+                                scope="col">
+                                {label}
+                            </th>
+                        )
+                    })}
+                </tr>
+            </thead>
+            <tbody>
+                {registeredTanks.map(({ unimedId, cyclinderSerialNumber,
+                    cylinderSize, origin, owner }, key) => {
+                    return (
+                        <React.Fragment key={key}>
+                            <tr>
+                                <td>{unimedId}</td>
+                                <td>{cylinderSize}</td>
+                                <td>{cyclinderSerialNumber}</td>
+                                <td>{origin}</td>
+                                <td>{owner}</td>
+                            </tr>
+                        </React.Fragment>
+                    )
+                })}
+            </tbody>
+        </table>
     )
 }
 
