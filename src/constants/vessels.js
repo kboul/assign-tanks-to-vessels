ab08688ee31ef77445513a91ce77d608493ca3ca
kboul <kos.voulgaridis@gmail.com>
2019-04-05 12:25:40 +0300

    Create vesselList constant

---
+++ src/constants/vessels.js
@@ -0,0 +1,8 @@
+export const vesselList = [
+    { name: 'Astoria', flag: 'Greece' },
+    { name: 'Lady Gloria', flag: 'Malta' },
+    { name: '17th October', flag: 'Netherlands' },
+    { name: 'Jela Delta', flag: 'Greece' },
+    { name: 'Claudia', flag: 'Malta' },
+    { name: 'Janna', flag: 'Denmark' }
+]
