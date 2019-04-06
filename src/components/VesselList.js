f15f49f4d2f98f4b9d3c040134e62cc7e54916c0
kboul <kos.voulgaridis@gmail.com>
2019-04-06 09:08:07 +0300

    Make VesselList a class based component to be able to highlight a table row to select a vessel

--- src/components/VesselList.js
+++ src/components/VesselList.js
@@ -1,39 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { vesselList } from '../constants/vessels'
 import { selectVesselGrid } from '../redux-local/actions'
+import { hightlightTableRow } from '../utils/highlightTableRow'
 
-const VesselList = ({ selectVesselGrid }) => {
-    return (
-        <div className="mt-4 mb-4">
-            <h4 >Vessel List</h4>
-            <table className="table mt-2" id="tanksTable">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Flag</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {vesselList.map(({ name, flag }, key) => {
-                        return (
-                            <tr style={{ cursor: 'pointer' }}
-                                key={key}
-                                onClick={() => {
-                                    selectVesselGrid(name)
-                                    console.log(key)
-                                }
-                                }>
-                                <td>{name}</td>
-                                <td>{flag}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-        </div>
-    )
+class VesselList extends Component {
+    state = {
+        currentRowIndex: ''
+    }
+
+    handleChangeId = currentRowIndex => {
+        this.setState({ currentRowIndex })
+    }
+
+    render() {
+        return (
+            <div className="mt-4 mb-4">
+                <h4 >Vessel List</h4>
+                <table className="table mt-2" id="tanksTable">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Flag</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {vesselList.map(({ name, flag }, index) => {
+                            return (
+                                <tr
+                                    style={hightlightTableRow(index, this.state.currentRowIndex)}
+                                    key={index}
+                                    onClick={() => {
+                                        this.handleChangeId(index)
+                                        this.props.selectVesselGrid(name)
+                                    }}>
+                                    <td>{name}</td>
+                                    <td>{flag}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
 }
 
 VesselList.propTypes = {
