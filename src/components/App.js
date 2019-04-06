9ddef844c838655a62a2080459b936522e6ede41
kboul <kos.voulgaridis@gmail.com>
2019-04-06 17:59:03 +0300

    Remove some uneccessary bootstrap classes from App

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,7 @@ import Container from './Container'
 const App = () => {
     return (
         <div className="container mt-2">
-            <div className="row">
-                <div className="col-md-12">
-                    <Container />
-                </div>
-            </div>
+            <Container />
         </div>
     )
 }
