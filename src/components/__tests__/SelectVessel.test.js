238729e161d0b9878c1d6ff5cf7b13c7a4e71e25
kboul <kos.voulgaridis@gmail.com>
2019-04-07 21:24:28 +0300

    Unit test Select comp

---
+++ src/components/__tests__/SelectVessel.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Rout from '../../Rout'
+import SelectVessel from '../SelectVessel'
+import Select from '../../common/Select'
+
+let wrapped
+
+beforeEach(() => {
+    wrapped = mount(
+        <Rout>
+            <SelectVessel />
+        </Rout>
+    )
+})
+
+afterEach(() => wrapped.unmount())
+
+it('contains <Select />', () => {
+    expect(wrapped.find(Select)).toHaveLength(1)
+})
+
+it('contains <Select /> and two props are passed', () => {
+    expect(wrapped.find(Select).prop('label')).toBeDefined()
+    expect(wrapped.find(Select).prop('name')).toBeDefined()
+})
