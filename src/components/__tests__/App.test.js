cb5e967fcd31fe0aea2de967ef40b529e74671fd
kboul <kos.voulgaridis@gmail.com>
2019-04-07 16:02:34 +0300

    Unit test App

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from '../App'
+import Container from '../Container'
+
+let wrapped
+
+beforeEach(() => {
+    wrapped = shallow(<App />)
+})
+
+it('shows <Container />', () => {
+    expect(wrapped.find(Container).length).toEqual(1)
+})
