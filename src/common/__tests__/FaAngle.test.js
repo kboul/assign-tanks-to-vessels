7b4891c333c9e9abccecb115507d4f7d151070ea
kboul <kos.voulgaridis@gmail.com>
2019-04-07 23:00:26 +0300

    Unit tests for FaAngle

---
+++ src/common/__tests__/FaAngle.test.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Rout from '../../Rout'
+import FaAngle from '../FaAngle';
+
+
+describe('prop isAccordionCollapsed is passed as true', () => {
+    const wrapped = shallow(
+        <FaAngle isAccordionCollapsed={true} />
+    )
+
+    it('contains fa-angle-down class', () => {
+        expect(wrapped.find('i').prop('className')).toContain('fa-angle-down')
+    })
+})
+
+describe('prop isAccordionCollapsed is passed as false', () => {
+    const wrapped = shallow(
+        <FaAngle isAccordionCollapsed={false} />
+    )
+
+    it('contains fa-angle-up class', () => {
+        expect(wrapped.find('i').prop('className')).toContain('fa-angle-up')
+    })
+})
