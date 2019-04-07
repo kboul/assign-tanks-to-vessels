9ee6a1d41de0d1f3ad20853f2e26309ee282ff48
kboul <kos.voulgaridis@gmail.com>
2019-04-07 22:32:09 +0300

    Unit test oxygenTankPerFlag

---
+++ src/utils/__tests__/oxygenTanksPerFlag.test.js
@@ -0,0 +1,35 @@
+import { oxygenTankPerFlag } from '../oxygenTanksPerFlag'
+
+const tanks = [{
+    unimedId: "666",
+    cyclinderSerialNumber: "6",
+    cylinderSize: "MOX-40",
+    origin: "US",
+    owner: "UNIMED",
+    vessel: 'Astoria',
+    flag: 'Greece'
+},
+{
+    unimedId: "123",
+    cyclinderSerialNumber: "123",
+    cylinderSize: "MOX-40",
+    origin: "US",
+    owner: "UNIMED",
+    vessel: 'Jela Delta',
+    flag: 'Greece'
+},
+{
+    unimedId: "123",
+    cyclinderSerialNumber: "123",
+    cylinderSize: "MOX-5",
+    origin: "US",
+    owner: "UNIMED",
+    vessel: 'Claudia',
+    flag: 'Malta'
+}]
+
+it(`returns the number of registered tanks 
+    that have the provided (as function arguments) 
+    flag & cylinder type`, () => {
+        expect(oxygenTankPerFlag(tanks, 'Greece', 'MOX-40')).toEqual(2)
+    })
