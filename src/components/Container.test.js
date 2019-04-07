f712ae6c2c16c79f17fba71fb0922c6887ad18c9
kboul <kos.voulgaridis@gmail.com>
2019-04-07 21:15:09 +0300

    Write some unit tests for Container

---
+++ src/components/Container.test.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Rout from '../Rout'
+import Container from './Container'
+import VesselList from './VesselList'
+import VesselsGrid from './VesselsGrid'
+import TankList from './TankList'
+
+let wrapped
+
+beforeEach(() => {
+    wrapped = mount(
+        <Rout>
+            <Container />
+        </Rout>
+    )
+
+})
+
+it('contains <TankList />', () => {
+    expect(wrapped.find(TankList)).toHaveLength(1)
+})
+
+it('contains <VesselList />', () => {
+    expect(wrapped.find(VesselList)).toHaveLength(1)
+})
+
+it('contains <VesselsGrid />', () => {
+    expect(wrapped.find(VesselsGrid)).toHaveLength(1)
+})
