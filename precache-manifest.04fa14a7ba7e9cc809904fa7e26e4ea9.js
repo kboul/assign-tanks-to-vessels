9aca3d31f1d6b7552e0a0662fd237f11e95b7c0b
kboul <kos.voulgaridis@gmail.com>
2019-04-25 12:52:06 +0300

    Updates

---
+++ precache-manifest.04fa14a7ba7e9cc809904fa7e26e4ea9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "14f37d6c869187192ce7",
+    "url": "/umm-frontend-assignment/static/css/main.54eb01c8.chunk.css"
+  },
+  {
+    "revision": "14f37d6c869187192ce7",
+    "url": "/umm-frontend-assignment/static/js/main.fe602d01.chunk.js"
+  },
+  {
+    "revision": "4c52ff4a1b903a5ee24f",
+    "url": "/umm-frontend-assignment/static/js/runtime~main.6244fc09.js"
+  },
+  {
+    "revision": "f82a1993d45972edebe2",
+    "url": "/umm-frontend-assignment/static/css/2.5c4b8840.chunk.css"
+  },
+  {
+    "revision": "f82a1993d45972edebe2",
+    "url": "/umm-frontend-assignment/static/js/2.ac6d54fa.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/umm-frontend-assignment/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/umm-frontend-assignment/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/umm-frontend-assignment/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/umm-frontend-assignment/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/umm-frontend-assignment/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "f733e880473473cca9e9169cb23d0302",
+    "url": "/umm-frontend-assignment/index.html"
+  }
+];
